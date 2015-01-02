1a1292fc4ba457be48ac9e5c255d9fe2df39f6ed
Pshriwise <shriwise@wisc.edu>
2015-01-02 10:29:53 -0600

    Initial commit. This should include all extensions I've added to a starter swift project to get data from the AUDL server. Added a git ignore to avoid binary files. Not sure if that's working yet or not.

---
+++ TopApps-Starter/TopApps/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  TopApps
+//
+//  Created by Dani Arnaout on 9/1/14.
+//  Edited by Eric Cerney on 9/27/14.
+//  Copyright (c) 2014 Ray Wenderlich All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Get the #1 app name from iTunes and SwiftyJSON
+        DataManager.getTopAppsDataFromItunesWithSuccess { (iTunesData) -> Void in
+            let json = JSON(data: iTunesData)
+            for i in 0...9 {
+            if let TeamName = json[i][0].stringValue {
+                if let TeamID = json[i][1].stringValue {
+                println("Team \(i): \(TeamName) \(TeamID)")
+             }
+            }
+            }
+            // More soon...
+        }
+}
+
+}
