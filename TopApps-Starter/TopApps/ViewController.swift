d00a9da242dcf18155ed876b5528f587d2b813bb
Pshriwise <shriwise@wisc.edu>
2015-01-05 22:10:11 -0600

    Got a table view setup with team names.

--- TopApps-Starter/TopApps/ViewController.swift
+++ TopApps-Starter/TopApps/ViewController.swift
@@ -10,23 +10,64 @@
 import UIKit
 
 
-class ViewController: UIViewController {
-  
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet
+    var tableView: UITableView!
+
+    var items: [String] = ["hot","hot","mama"]
+    
+    var num_rows = 0
+    
+    var json:JSON = JSON.Null(nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         // Get the #1 app name from iTunes and SwiftyJSON
-        DataManager.getUrlDataWithSuccess { (iTunesData) -> Void in
-            let json = JSON(data: iTunesData)
+        DataManager.getUrlDataWithSuccess { (Data) -> Void in
+            let json = JSON(data: Data)
             for i in 0...9 {
             if let TeamName = json[i][0].stringValue {
                 if let TeamID = json[i][1].stringValue {
                 println("Team \(i): \(TeamName) \(TeamID)")
-             }
+                }
             }
-            }
-            // More soon...
         }
-}
+            
+        // More soon...
+        }
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        
+    }
+
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 20
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+
+        DataManager.getUrlDataWithSuccess { (Data) -> Void in
+            let json = JSON(data: Data)
+            println(json[indexPath.row])
+            println(indexPath.row)
+            cell.textLabel?.text = json[indexPath.row][0].stringValue
+            
+        }
+    
+//        cell.textLabel?.text = self.items[indexPath.row]//[0].stringValue
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 
 }
